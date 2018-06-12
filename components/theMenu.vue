6d6233daf8575de0a02f74d25f76769b42a966de
Thomas Sanlis <Thomas.sanlis@gmail.com>
2018-06-12 20:11:05 +0200

    :lipstick: Add navbar

--- components/theMenu.vue
+++ components/theMenu.vue
@@ -2,7 +2,7 @@
   <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+        <img src="~/assets/logo.png">
       </a>
       <div class="navbar-burger burger" @click="toggleBurger" :class="{'is-active': isBurgerActive}" data-target="navbar-Lstnr">
         <span aria-hidden="true"></span>
@@ -10,82 +10,76 @@
         <span aria-hidden="true"></span>
       </div>
     </div>
-
     <div id="navbar-Lstnr" :class="{'is-active': isBurgerActive}" class="navbar-menu">
-      <div class="navbar-end">
+      <div class="navbar-start">
         <div class="navbar-item">
-          <a class="navbar-item" href="#">
+          <b-field>
+            <b-input placeholder="Search" type="search" icon="magnify">
+            </b-input>
+          </b-field>
+        </div>
+        <div class="navbar-item">
+          <a href="#">
             Home
           </a>
         </div>
         <div class="navbar-item">
-          <a class="navbar-item" href="#">
+          <a href="#">
             Platforms
           </a>
         </div>
         <div class="navbar-item">
-          <a class="navbar-item" href="#">
+          <a href="#">
             Our roadmap
           </a>
         </div>
         <div class="navbar-item">
-          <a class="navbar-item" href="#">
+          <a href="#">
             FAQ
           </a>
         </div>
         <div class="navbar-item" v-if="!user">
-            <Login></Login>
+          <Login></Login>
         </div>
         <div class="navbar-item" v-if="!user">
-            <Register></Register>
+          <Register></Register>
         </div>
+      </div>
+      <div class="navbar-end">
         <div class="navbar-item is-hoverable" v-if="user">
-            <div class="field is-grouped">
-              <figure class="image is-24x24">
-                <img class="avatar" src="https://bulma.io/images/placeholders/24x24.png">
-              </figure>
-              <div class="user">
-                {{ user.username }}
-              </div>
-
-              <div class="navbar-dropdown is-boxed">
-                <a class="navbar-item" href="#">
-                  Profile
-                </a>
-                <a class="navbar-item" href="#">
-                  Settings
-                </a>
-              </div>
+          <div class="field is-grouped">
+            <figure class="image is-24x24">
+              <img class="avatar" src="https://bulma.io/images/placeholders/24x24.png">
+            </figure>
+            <div class="user">
+              {{ user.username }}
             </div>
+
+            <div class="navbar-dropdown is-boxed">
+              <a class="navbar-item" href="#">
+                Profile
+              </a>
+              <a class="navbar-item" href="#">
+                Settings
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
-<style>
-  nav {
-    margin-bottom: 20px;
-  }
-  .avatar {
-    border-radius: 50%;
-    border: 2px solid purple;
-  }
-  .user {
-    padding-left: 10px;
-  }
-</style>
-
 <script>
 import { mapState } from 'vuex'
-import Login from './login.vue';
-import Register from './register.vue';
+import Login from './login.vue'
+import Register from './register.vue'
 export default {
   name: 'theMenu',
   data() {
     return {
       test: 'empty',
-      isBurgerActive: false,
+      isBurgerActive: false
     }
   },
   computed: {
@@ -93,7 +87,7 @@ export default {
   },
   methods: {
     toggleBurger() {
-      this.$data.isBurgerActive = !this.$data.isBurgerActive;
+      this.$data.isBurgerActive = !this.$data.isBurgerActive
     }
   },
   components: {
@@ -102,3 +96,41 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.navbar {
+  height: 6rem;
+  .navbar-menu {
+    .navbar-start {
+      .navbar-item {
+        .field {
+          margin: 0 1rem;
+          input {
+            background-color: #f6f6f6;
+            color: #9b9b9b;
+            border-radius: 4px;
+            border-color: transparent;
+            box-shadow: none;
+          }
+        }
+        a {
+          font-size: 0.8rem;
+          font-family: 'Futura Bold';
+          color: #1f1f1f;
+        }
+      }
+    }
+    .navbar-end {
+      .navbar-item {
+        figure {
+          margin-right: 1rem;
+        }
+        .navbar-dropdown {
+          top: calc(100% + (-1rem));
+        }
+      }
+    }
+  }
+}
+</style>
+
